43f5e2d9e9449770859b9343bac61d65f9480898
2037b <1027807976@qq.com>
2020-01-13 13:07:18 +0800

    modify

---
+++ src/main/java/club/banyuan/IndexController.java
@@ -0,0 +1,16 @@
+package club.banyuan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+  @GetMapping("/{username}")
+  @ResponseBody
+  String getUserName(@PathVariable String username) {
+    return "user:" + username;
+  }
+}
